514864e358ee17e898baed2f80235a2001cf13f3
OutOfYourMind <outofyourmindyt@gmail.com>
2016-02-06 13:43:22 -0500

    Update TFM_ConfigEntry.java

--- src/me/StevenLawson/TotalFreedomMod/Config/TFM_ConfigEntry.java
+++ src/me/StevenLawson/TotalFreedomMod/Config/TFM_ConfigEntry.java
@@ -33,6 +33,11 @@ public enum TFM_ConfigEntry
     SERVER_ADDRESS(String.class, "server.address"),
     SERVER_MOTD(String.class, "server.motd"),
     SERVER_OWNERS(List.class, "server.owners"),
+    SERVER_COOWNERS(List.class, "server.coowners"),
+    SERVER_LEADDEVS(List.class, "server.leaddevs"),
+    SERVER_SFDEVS(List.class, "server.sfdevs"),
+    SERVER_SYSTEMADMINS(List.class, "server.systemadmins"),
+    SERVER_EXEC(List.class, "server.execs"),
     SERVER_BAN_URL(String.class, "server.ban_url"),
     SERVER_PERMBAN_URL(String.class, "server.permban_url"),
     //
